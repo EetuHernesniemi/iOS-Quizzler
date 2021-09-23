fc00d29eb2f2181b42d1f1e68cd82cbda6f6c82d
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-09-23 15:21:19 +0300

    question structure. more questions

---
+++ Quizzler/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Eetu Hernesniemi on 23.9.2021.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+}
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -14,9 +14,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     
     let quiz = [
-        ["Four + Two is equal to Six.", "True"],
-        ["Five - Three is greater than One", "True"],
-        ["Three + Eight is less than Ten", "False"]
+        Question(text: "A slug's blood is green.", answer: "True"),
+              Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
+              Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
+              Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
+              Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
+              Question(text: "It is illegal to pee in the Ocean in Portugal.", answer: "True"),
+              Question(text: "You can lead a cow down stairs but not up stairs.", answer: "False"),
+              Question(text: "Google was originally called 'Backrub'.", answer: "True"),
+              Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
+              Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
+              Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
+              Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True")
     ]
     
     var currentQuestionNumber = -1
@@ -29,7 +38,7 @@ class ViewController: UIViewController {
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         let userAnswer : String = sender.currentTitle!
-        let actualAnswer : String = quiz[currentQuestionNumber][1]
+        let actualAnswer : String = quiz[currentQuestionNumber].answer
         if userAnswer == actualAnswer {
             print("Correct!")
             currentScore += 1
@@ -42,7 +51,7 @@ class ViewController: UIViewController {
     func loadNextQuestion() {
         if currentQuestionNumber < (quiz.count - 1) {
             currentQuestionNumber += 1
-            questionLabel.text = quiz[currentQuestionNumber][0]
+            questionLabel.text = quiz[currentQuestionNumber].text
         } else {
             finishQuizzler()
         }
@@ -56,7 +65,7 @@ class ViewController: UIViewController {
     func restartQuizzler() {
         currentScore = 0
         currentQuestionNumber = 0
-        questionLabel.text = quiz[currentQuestionNumber][0]
+        questionLabel.text = quiz[currentQuestionNumber].text
     }
     
 }
