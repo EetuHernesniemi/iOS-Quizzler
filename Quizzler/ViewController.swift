2020af61008a6532c189b3efa5333c53f2a164f1
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-09-23 11:14:42 +0300

    checking if user answer is correct. Incrementing score on right answer.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -14,12 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     
     let quiz = [
-        "Four + Two is equal to Six.",
-        "Five - Three is greater than One",
-        "Three + Eight is less than Ten"
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is greater than One", "True"],
+        ["Three + Eight is less than Ten", "False"]
     ]
     
     var currentQuestionNumber = -1
+    var currentScore = 0
     
     override func viewDidLoad() {
         loadNextQuestion()
@@ -27,16 +28,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer : String = sender.currentTitle!
+        let actualAnswer : String = quiz[currentQuestionNumber][1]
+        if userAnswer == actualAnswer {
+            print("Correct!")
+            currentScore += 1
+        } else {
+            print("Incorrect!")
+        }
         loadNextQuestion()
     }
     
     func loadNextQuestion() {
         currentQuestionNumber += 1
         if currentQuestionNumber < quiz.count {
-            questionLabel.text = quiz[currentQuestionNumber]
+            questionLabel.text = quiz[currentQuestionNumber][0]
         } else {
             currentQuestionNumber -= 1 //revert the previous currentQuestionNumber increment.
+            finishQuizzler()
         }
+    }
+    
+    func finishQuizzler() {
+        print("Your final score is \(currentScore)")
     }
     
 }
