a5299a00a83ce68636caedb25c150b99f95498a9
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-09-23 10:54:10 +0300

    shift to next questions by button click.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var trueButton: UIButton!
+    
+    let quiz = [
+        "Four + Two is equal to Six.",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ]
+    
+    var currentQuestionNumber = -1
+    
     override func viewDidLoad() {
+        loadNextQuestion()
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        loadNextQuestion()
+    }
+    
+    func loadNextQuestion() {
+        currentQuestionNumber += 1
+        if currentQuestionNumber < quiz.count {
+            questionLabel.text = quiz[currentQuestionNumber]
+        } else {
+            currentQuestionNumber -= 1 //revert the previous currentQuestionNumber increment.
+        }
+    }
+    
 }
 
