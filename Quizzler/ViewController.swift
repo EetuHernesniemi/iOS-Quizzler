62e101ac97e0617b8da9184241ee24914f319d81
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-09-24 10:08:03 +0300

    update progress bar on advancing questions

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -16,17 +16,17 @@ class ViewController: UIViewController {
     
     let quiz = [
         Question(text: "A slug's blood is green.", answer: "True"),
-              Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
-              Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
-              Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
-              Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
-              Question(text: "It is illegal to pee in the Ocean in Portugal.", answer: "True"),
-              Question(text: "You can lead a cow down stairs but not up stairs.", answer: "False"),
-              Question(text: "Google was originally called 'Backrub'.", answer: "True"),
-              Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
-              Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
-              Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
-              Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True")
+        Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
+        Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
+        Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
+        Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
+        Question(text: "It is illegal to pee in the Ocean in Portugal.", answer: "True"),
+        Question(text: "You can lead a cow down stairs but not up stairs.", answer: "False"),
+        Question(text: "Google was originally called 'Backrub'.", answer: "True"),
+        Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
+        Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
+        Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
+        Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True")
     ]
     
     var currentQuestionNumber = -1
@@ -55,6 +55,7 @@ class ViewController: UIViewController {
         clearColorOfButtons()
         if currentQuestionNumber < (quiz.count - 1) {
             currentQuestionNumber += 1
+            updateProgressBar()
             questionLabel.text = quiz[currentQuestionNumber].text
         } else {
             finishQuizzler()
@@ -70,6 +71,7 @@ class ViewController: UIViewController {
         currentScore = 0
         currentQuestionNumber = 0
         questionLabel.text = quiz[currentQuestionNumber].text
+        updateProgressBar()
     }
     
     func callLoadNextQuestionWithDelay(){
@@ -82,6 +84,10 @@ class ViewController: UIViewController {
     func clearColorOfButtons() {
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
+    }
+    
+    func updateProgressBar(){
+        progressBar.progress = Float(currentQuestionNumber) / Float(quiz.count)
     }
     
 }
