2215f59740d32b9ca503a4eda9019f316013cfd9
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2021-09-24 13:51:20 +0300

    quizBrain model to handle app state

--- Quizzler/Controller/ViewController.swift
+++ Quizzler/Controller/ViewController.swift
@@ -13,24 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var trueButton: UIButton!
     var timer = Timer()
-    
-    let quiz = [
-        Question(text: "A slug's blood is green.", answer: "True"),
-        Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
-        Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
-        Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
-        Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
-        Question(text: "It is illegal to pee in the Ocean in Portugal.", answer: "True"),
-        Question(text: "You can lead a cow down stairs but not up stairs.", answer: "False"),
-        Question(text: "Google was originally called 'Backrub'.", answer: "True"),
-        Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
-        Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
-        Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
-        Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True")
-    ]
-    
-    var currentQuestionNumber = -1
-    var currentScore = 0
+    var quizBrain = QuizBrain()
     
     override func viewDidLoad() {
         loadNextQuestion()
@@ -39,11 +22,11 @@ class ViewController: UIViewController {
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         let userAnswer : String = sender.currentTitle!
-        let actualAnswer : String = quiz[currentQuestionNumber].answer
-        if userAnswer == actualAnswer {
+        let userGotItRight : Bool = quizBrain.checkAnswer(userAnswer)
+        if userGotItRight == true {
             print("Correct!")
             sender.backgroundColor = UIColor.green
-            currentScore += 1
+            quizBrain.currentUserScore += 1
         } else {
             print("Incorrect!")
             sender.backgroundColor = UIColor.red
@@ -53,25 +36,25 @@ class ViewController: UIViewController {
     
     func loadNextQuestion() {
         clearColorOfButtons()
-        if currentQuestionNumber < (quiz.count - 1) {
-            currentQuestionNumber += 1
-            updateProgressBar()
-            questionLabel.text = quiz[currentQuestionNumber].text
+        if quizBrain.currentQuestionNumber < (quizBrain.quiz.count - 1) {
+            quizBrain.currentQuestionNumber += 1
+            progressBar.progress = quizBrain.getProgressBarProgress()
+            questionLabel.text = quizBrain.getQuestionText()
         } else {
             finishQuizzler()
         }
     }
     
     func finishQuizzler() {
-        print("Your final score is \(currentScore)")
+        print("Your final score is \(quizBrain.currentUserScore)")
         restartQuizzler()
     }
     
     func restartQuizzler() {
-        currentScore = 0
-        currentQuestionNumber = 0
-        questionLabel.text = quiz[currentQuestionNumber].text
-        updateProgressBar()
+        quizBrain.currentUserScore = 0
+        quizBrain.currentQuestionNumber = 0
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgressBarProgress()
     }
     
     func callLoadNextQuestionWithDelay(){
@@ -85,16 +68,5 @@ class ViewController: UIViewController {
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
     }
-    
-    func updateProgressBar(){
-        let divideByVal : Float = (Float(quiz.count) - 1.0)
-        if divideByVal > 0 {
-            progressBar.progress = Float(currentQuestionNumber) / divideByVal
-        } else {
-            progressBar.progress = 0
-        }
-        
-    }
-    
 }
 
---
+++ Quizzler/Model/QuizBrain.swift
@@ -0,0 +1,49 @@
+//
+//  QuizBrain.swift
+//  Quizzler
+//
+//  Created by Eetu Hernesniemi on 24.9.2021.
+//
+
+import Foundation
+
+struct QuizBrain {
+    let quiz = [
+        Question(text: "A slug's blood is green.", answer: "True"),
+        Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
+        Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
+        Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
+        Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
+        Question(text: "It is illegal to pee in the Ocean in Portugal.", answer: "True"),
+        Question(text: "You can lead a cow down stairs but not up stairs.", answer: "False"),
+        Question(text: "Google was originally called 'Backrub'.", answer: "True"),
+        Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
+        Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
+        Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
+        Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True")
+    ]
+    var currentUserScore = 0
+    var currentQuestionNumber = -1
+    
+    func checkAnswer(_ userAnswer: String) -> Bool {
+        if userAnswer == quiz[currentQuestionNumber].answer {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func getQuestionText() -> String {
+        return quiz[currentQuestionNumber].text
+    }
+    
+    func getProgressBarProgress() -> Float {
+        let divideByVal : Float = (Float(quiz.count) - 1.0)
+        if divideByVal > 0 {
+            return Float(currentQuestionNumber) / divideByVal
+        } else {
+            return 0
+        }
+        
+    }
+}
